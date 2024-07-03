cbde6f728b0f3650ad43617f5061c984b60fba33
devscopee <scopedeveloper31@gmail.com>
2024-07-03 11:06:24 +0400

    a

--- app/(child)/c/page.js
+++ app/(child)/c/page.js
@@ -1,5 +1,23 @@
 'use client'
+import {useRouter} from 'next/navigation'
+
 const page = () => {
+  const router = useRouter();
+  useEffect(() => {
+    if (
+      localStorage.getItem("email") &&
+      localStorage.getItem("password") &&
+      localStorage.getItem("type") === "parent"
+    ) {
+      router.push("/p/dashboard/");
+    } else if (
+      localStorage.getItem("email") &&
+      localStorage.getItem("password") &&
+      localStorage.getItem("type") === "child"
+    ) {
+      router.push("/c/");
+    }
+  }, [router]);
   return (
     <div>page</div>
   )
